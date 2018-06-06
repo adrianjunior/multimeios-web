f8ba894a5cba633b8a21d7987c35c022020da92f
adrianjunior <adrianrfpj@gmail.com>
2018-06-06 01:55:39 -0300

    lista de livros add

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy} from '@angular/core';
+import { Router } from '@angular/router';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+
+import { Book } from '../book.model';
+import books from '../books';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns = ['title', 'author', 'gender', 'available'];
+  dataSource = new MatTableDataSource<Book>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private router: Router) {}
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
 
   ngOnInit() {
+    this.dataSource.data = books;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onGoToProfile(id) {
+    this.router.navigateByUrl('perfil-do-livro/' + id);
+    console.log(id);
   }
 
 }
