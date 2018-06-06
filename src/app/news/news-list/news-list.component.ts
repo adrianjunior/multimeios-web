9b3ce37fafa1e7126c3cd180368b15b2b530ae60
adrianjunior <adrianrfpj@gmail.com>
2018-06-06 02:19:11 -0300

    lista de noticias add

--- src/app/news/news-list/news-list.component.ts
+++ src/app/news/news-list/news-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+
+import { News } from '../news.model';
+import news from '../news';
 
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
   styleUrls: ['./news-list.component.css']
 })
-export class NewsListComponent implements OnInit {
+export class NewsListComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns = ['title', 'date', 'time'];
+  dataSource = new MatTableDataSource<News>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private router: Router) {}
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
 
   ngOnInit() {
+    this.dataSource.data = news;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onGoToProfile(id) {
+    this.router.navigateByUrl('perfil-noticia/' + id);
+    console.log(id);
   }
 
 }
--- src/app/news/news.ts
+++ src/app/news/news.ts
@@ -1,5 +1,6 @@
 export default [
     {
+        id: 'news1',
         title: 'Título da Notícia 1',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
@@ -7,54 +8,63 @@ export default [
 
     },
     {
+        id: 'news2',
         title: 'Título da Notícia 2',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
         body: 'Praesent sagittis suscipit nunc in tincidunt. Nulla posuere quam ante, nec tincidunt arcu imperdiet ut. Sed id viverra ex. Aliquam vel enim ac sem tristique egestas et vitae erat. Sed nulla diam, facilisis sit amet rutrum et, tincidunt sed diam. Maecenas laoreet nisl semper vehicula lobortis. Nunc lacinia purus eget cursus convallis. In lobortis viverra ante a blandit. Curabitur aliquam neque molestie bibendum dapibus. Quisque et ante porttitor, tempor turpis nec, laoreet quam.'
     },
     {
+        id: 'news3',
         title: 'Título da Notícia 3',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
         body: 'Praesent sagittis suscipit nunc in tincidunt. Nulla posuere quam ante, nec tincidunt arcu imperdiet ut. Sed id viverra ex. Aliquam vel enim ac sem tristique egestas et vitae erat. Sed nulla diam, facilisis sit amet rutrum et, tincidunt sed diam. Maecenas laoreet nisl semper vehicula lobortis. Nunc lacinia purus eget cursus convallis. In lobortis viverra ante a blandit. Curabitur aliquam neque molestie bibendum dapibus. Quisque et ante porttitor, tempor turpis nec, laoreet quam.'
     },
     {
+        id: 'news4',
         title: 'Título da Notícia 4',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
         body: 'Praesent sagittis suscipit nunc in tincidunt. Nulla posuere quam ante, nec tincidunt arcu imperdiet ut. Sed id viverra ex. Aliquam vel enim ac sem tristique egestas et vitae erat. Sed nulla diam, facilisis sit amet rutrum et, tincidunt sed diam. Maecenas laoreet nisl semper vehicula lobortis. Nunc lacinia purus eget cursus convallis. In lobortis viverra ante a blandit. Curabitur aliquam neque molestie bibendum dapibus. Quisque et ante porttitor, tempor turpis nec, laoreet quam.'
     },
     {
+        id: 'news5',
         title: 'Título da Notícia 5',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
         body: 'Praesent sagittis suscipit nunc in tincidunt. Nulla posuere quam ante, nec tincidunt arcu imperdiet ut. Sed id viverra ex. Aliquam vel enim ac sem tristique egestas et vitae erat. Sed nulla diam, facilisis sit amet rutrum et, tincidunt sed diam. Maecenas laoreet nisl semper vehicula lobortis. Nunc lacinia purus eget cursus convallis. In lobortis viverra ante a blandit. Curabitur aliquam neque molestie bibendum dapibus. Quisque et ante porttitor, tempor turpis nec, laoreet quam.'
     },
     {
+        id: 'news6',
         title: 'Título da Notícia 6',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
         body: 'Praesent sagittis suscipit nunc in tincidunt. Nulla posuere quam ante, nec tincidunt arcu imperdiet ut. Sed id viverra ex. Aliquam vel enim ac sem tristique egestas et vitae erat. Sed nulla diam, facilisis sit amet rutrum et, tincidunt sed diam. Maecenas laoreet nisl semper vehicula lobortis. Nunc lacinia purus eget cursus convallis. In lobortis viverra ante a blandit. Curabitur aliquam neque molestie bibendum dapibus. Quisque et ante porttitor, tempor turpis nec, laoreet quam.'
     },
     {
+        id: 'news7',
         title: 'Título da Notícia 7',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
         body: 'Praesent sagittis suscipit nunc in tincidunt. Nulla posuere quam ante, nec tincidunt arcu imperdiet ut. Sed id viverra ex. Aliquam vel enim ac sem tristique egestas et vitae erat. Sed nulla diam, facilisis sit amet rutrum et, tincidunt sed diam. Maecenas laoreet nisl semper vehicula lobortis. Nunc lacinia purus eget cursus convallis. In lobortis viverra ante a blandit. Curabitur aliquam neque molestie bibendum dapibus. Quisque et ante porttitor, tempor turpis nec, laoreet quam.'
     },
     {
+        id: 'news8',
         title: 'Título da Notícia 8',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
         body: 'Praesent sagittis suscipit nunc in tincidunt. Nulla posuere quam ante, nec tincidunt arcu imperdiet ut. Sed id viverra ex. Aliquam vel enim ac sem tristique egestas et vitae erat. Sed nulla diam, facilisis sit amet rutrum et, tincidunt sed diam. Maecenas laoreet nisl semper vehicula lobortis. Nunc lacinia purus eget cursus convallis. In lobortis viverra ante a blandit. Curabitur aliquam neque molestie bibendum dapibus. Quisque et ante porttitor, tempor turpis nec, laoreet quam.'
     },
     {
+        id: 'news9',
         title: 'Título da Notícia 9',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
         body: 'Praesent sagittis suscipit nunc in tincidunt. Nulla posuere quam ante, nec tincidunt arcu imperdiet ut. Sed id viverra ex. Aliquam vel enim ac sem tristique egestas et vitae erat. Sed nulla diam, facilisis sit amet rutrum et, tincidunt sed diam. Maecenas laoreet nisl semper vehicula lobortis. Nunc lacinia purus eget cursus convallis. In lobortis viverra ante a blandit. Curabitur aliquam neque molestie bibendum dapibus. Quisque et ante porttitor, tempor turpis nec, laoreet quam.'
     },
     {
+        id: 'news10',
         title: 'Título da Notícia 10',
         date: 'DD/MM/AAAA',
         time: 'HH:MM',
