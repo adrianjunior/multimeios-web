3d70cd3c6c911c4d5876a914c6f8a029414e0fe3
adrianjunior <adrianrfpj@gmail.com>
2018-06-06 12:10:01 -0300

    add registrar equip

--- src/app/equip/equip-register/equip-register.component.ts
+++ src/app/equip/equip-register/equip-register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Equip } from '../equip.model';
 
 @Component({
   selector: 'app-equip-register',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipRegisterComponent implements OnInit {
 
+  private equip: Equip;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    this.equip = {
+      id: 'dummy',
+      name: form.value.name,
+      type: form.value.type
+    }
+    console.log(this.equip);
   }
 
 }
