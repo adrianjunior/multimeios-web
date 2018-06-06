b4a9924598b25b6b3ae0b578cff8dedffbcddc96
adrianjunior <adrianrfpj@gmail.com>
2018-06-06 03:36:29 -0300

    registrar livro add

--- src/app/book/book-register/book-register.component.ts
+++ src/app/book/book-register/book-register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-book-register',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookRegisterComponent implements OnInit {
 
+  private book: Book;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    this.book = {
+      id: 'dummy',
+      title: form.value.title,
+      author: form.value.author,
+      quantity: form.value.quantity,
+      available: form.value.quantity,
+      editor: form.value.editor,
+      gender: form.value.gender,
+      edition: form.value.edition,
+      year: form.value.year
+    }
+    console.log(this.book);
   }
 
 }
