f0fd31b1eb6780ea7eb5a7143d59e35ce4a5beb2
adrianjunior <adrianrfpj@gmail.com>
2018-06-06 03:59:05 -0300

    registrar usuario add

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Student } from '../student.model';
+import { Teacher } from '../teacher.model';
+import { Staff } from '../staff.model';
 
 @Component({
   selector: 'app-user-register',
@@ -7,9 +12,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRegisterComponent implements OnInit {
 
+  private student: Student;
+  private teacher: Teacher;
+  private staff: Staff;
+
+  jobtypes = [
+    {value: 'Efetivo', viewValue: 'Efetivo'},
+    {value: 'Temporario', viewValue: 'Temporário'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmitStudent(form: NgForm) {
+    this.student = {
+      id: 'dummy',
+      name: form.value.name,
+      grade: form.value.grade,
+      class: form.value.class,
+      phone: form.value.phone,
+      email: form.value.email
+    };
+    console.log(this.student);
+  }
+
+  onSubmitTeacher(form: NgForm) {
+    this.teacher = {
+      id: 'dummy',
+      name: form.value.name,
+      role: form.value.role,
+      effective: form.value.effective,
+      phone: form.value.phone,
+      email: form.value.email
+    };
+    console.log(this.teacher);
+  }
+
+  onSubmitStaff(form: NgForm) {
+    this.staff = {
+      id: 'dummy',
+      name: form.value.name,
+      role: form.value.role,
+      phone: form.value.phone,
+      email: form.value.email
+    };
+    console.log(this.staff);
   }
 
 }
