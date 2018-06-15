cccd7f0ac90aa6a347b0da1a1e5e19882cd2824a
Álvaro <alvaro_carvalho@alu.ufc.br>
2018-06-15 13:59:54 -0300

    coisas

--- src/app/user/teachers.ts
+++ src/app/user/teachers.ts
@@ -1,66 +1,66 @@
 export default [
     {
         id: 'teacher1',
-        name: 'Professor 1',
-        role: 'Matéria 1',
+        name: 'Janaina Silva',
+        role: 'Física',
         effective: true,
         phone: '(11) 1111-1111',
-        email: 'professor1@email.com' 
+        email: 'janaina@gmail.com' 
     },
     {
         id: 'teacher2',
-        name: 'Professor 2',
-        role: 'Matéria 2',
+        name: 'Rogerio Costa',
+        role: 'Matemática',
         effective: false,
         phone: '(22) 2222-2222',
-        email: 'professor2@email.com' 
+        email: 'rsilva@hotmail.com' 
     },
     {
         id: 'teacher3',
-        name: 'Professor 3',
-        role: 'Matéria 3',
+        name: 'Carlos Santana',
+        role: 'Português',
         effective: true,
         phone: '(33) 3333-3333',
-        email: 'professor3@email.com' 
+        email: 'santana_guitarrista@yahoo.com' 
     },
     {
         id: 'teacher4',
-        name: 'Professor 4',
-        role: 'Matéria 4',
+        name: 'Claudia Batista',
+        role: 'Português',
         effective: false,
         phone: '(44) 4444-4444',
-        email: 'professor4@email.com' 
+        email: 'claudinhabatista@bol.com.br' 
     },
     {
         id: 'teacher5',
-        name: 'Professor 5',
-        role: 'Matéria 5',
+        name: 'João Menezes',
+        role: 'Química',
         effective: true,
         phone: '(55) 5555-5555',
-        email: 'professor5@email.com' 
+        email: 'joãozinho30@gmail.com' 
     },
     {
         id: 'teacher6',
-        name: 'Professor 6',
-        role: 'Matéria 6',
+        name: 'Raúl Figueiredo',
+        role: 'Artes',
         effective: false,
         phone: '(66) 6666-6666',
-        email: 'professor6@email.com' 
+        email: 'raulpicasso@hotmail.com' 
     },
     {
         id: 'teacher7',
-        name: 'Professor 7',
-        role: 'Matéria 7',
+        name: 'Emanuelle Flores',
+        role: 'Geografia',
         effective: true,
         phone: '(77) 7777-7777',
-        email: 'professor7@email.com' 
+        email: 'flores@gmail.com' 
     },
     {
         id: 'teacher8',
-        name: 'Professor 8',
-        role: 'Matéria 8',
+        name: 'Ronaldo Berto',
+        role: 'Biologia',
         effective: false,
         phone: '(88) 8888-8888',
-        email: 'professor8@email.com' 
+        email: 'ronaldobio@gmail.com' 
     },
 ]
